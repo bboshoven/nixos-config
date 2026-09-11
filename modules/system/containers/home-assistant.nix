3f9c0cfbbdd950cbac51a6719b5852cc77d7e343
bboshoven <boy@boshoven.dev>
2026-09-11 23:10:53 +0200

    Fix

--- modules/system/containers/home-assistant.nix
+++ modules/system/containers/home-assistant.nix
@@ -53,7 +53,7 @@
         environment = {
           RCP_HOST = "10.0.0.200";
           OTBR_BACKBONE_IF = "enp0s13f0u4u1";
-          OTBR_LOG_LEVEL_INT = "0";
+          OTBR_LOG_LEVEL_INT = "7";
           #OTBR_WEB_PORT = "56374";
           #OTBR_WEB_ENABLE = "1";
         };
