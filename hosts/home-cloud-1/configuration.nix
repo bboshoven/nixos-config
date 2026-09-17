ba5e4f2232fc7b8bc6e75fa2367a1c22bb30c2a1
bboshoven <boy@boshoven.dev>
2026-09-17 21:44:43 +0200

    Add hostId

--- hosts/home-cloud-1/configuration.nix
+++ hosts/home-cloud-1/configuration.nix
@@ -19,6 +19,7 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "home-cloud-1";
+  networking.hostId = "00000001";
 
   users.users.root.initialHashedPassword = "";
   users.users.root.openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMy3/7d3Vx4pF/+yrfHJFtrIL+tmI7wreyjEqh05ICl6 boy@boy-desktop"];
